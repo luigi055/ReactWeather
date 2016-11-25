fa58ea37c3f74f30ec34a594d4e8a676b395c417
Pedro La Rosa <luigi5021@gmail.com>
2016-11-24 21:31:07 -0400

    bug fixed

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 const express = require('express');
 
 //enviroment variable
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 //Create our App
-var app = express();
+const app = express();
 //redirect openweather map https traffic to http since we're using heroku
 //free service
+
 app.use((req, res, next) => {
-  (req.headers['x-forwarded-proto'] === 'http') ?
-    next() :
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
     res.redirect(`http://${req.hostname}${req.url}`);
+  }
 });
 
 app.use(express.static('public'));
 
 app.listen(PORT, () => {
-  console.log('express server is up on port ' + PORT);
+  console.log(`express server is up on port ${PORT}`);
 });
