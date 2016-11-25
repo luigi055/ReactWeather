1fd793a14f0910cf68e9527c51c5d09597cbeeb9
Pedro La Rosa <luigi5021@gmail.com>
2016-11-24 20:45:52 -0400

    optimizing web server

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 
-//Create our App
+//enviroment variable
+const PORT = process.env.PORT || 3000
 
+//Create our App
 var app = express();
+//redirect openweather map https traffic to http since we're using heroku
+//free service
+app.use((req, res, next) => {
+  (req.headers['x-forwarded-proto'] === 'http') ?
+    next() :
+    res.redirect(`http://${req.hostname}${req.url}`);
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, () => {
-  console.log('express server is up on port 3000');
+app.listen(PORT, () => {
+  console.log('express server is up on port ' + PORT);
 });
