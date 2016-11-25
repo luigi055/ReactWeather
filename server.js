a3ac899135bc5e93dac1932786532071da6ac2a7
Pedro La Rosa <luigi5021@gmail.com>
2016-11-24 23:56:23 -0400

    update middleware for localhost

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-var express = require('express');
+const express = require('express');
 
 // Create our app
-var app = express();
-const PORT = process.env.PORT || 3000;
-//change https to http
-// app.use(function (req, res, next){
-//   if (req.headers['x-forwarded-proto'] === 'http') {
-//     next();
-//   } else {
-//     res.redirect('http://' + req.hostname + req.url);
-//   }
-// });
+const app = express();
+const PORT = process.env.PORT || 5000;
+// change https to http req.headers[] doesn't exist locally
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect(`http://${req.hostname}${req.url}`);
+  } else {
+    next();
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(PORT, function () {
-  console.log('Express server is up on port ' + PORT);
+app.listen(PORT, () => {
+  console.log(`Express server is up on port ${PORT}`);
 });
