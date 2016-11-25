a3e93ab9c99a4ad47b744e13eec85af9202c20ac
Pedro La Rosa <luigi5021@gmail.com>
2016-11-24 21:44:26 -0400

    joder otra vez

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const PORT = process.env.PORT || 3000;
 
 //redirect openweather map https traffic to http since we're using heroku
 //free service
-app.use((req, res, next) => {
-  if (req.headers['x-forwarded-proto'] === 'http') {
-    next();
-  } else {
-    res.redirect(`http://${req.hostname}${req.url}`);
-  }
-});
+// app.use((req, res, next) => {
+//   if (req.headers['x-forwarded-proto'] === 'http') {
+//     next();
+//   } else {
+//     res.redirect(`http://${req.hostname}${req.url}`);
+//   }
+// });
 
 app.use(express.static('public'));
 
