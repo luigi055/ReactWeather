419d8c1879372a17fd1794d586fe57a6bd4f46a5
Pedro La Rosa <luigi5021@gmail.com>
2016-11-26 02:11:27 -0400

    style about page

--- app/components/About.js
+++ app/components/About.js
@@ -1,5 +1,23 @@
 import React from 'react';
 
-const About = props => <h3>About Section</h3>;
+const About = props => {
+ return (
+   <div>
+      <h3 className="text-center">About Section</h3>
+      <p>This is a weather application build on React, I have built this for The Complete react web developer course</p>
+      <p>
+        here are some of the tools i used:
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This was the javascript framework used
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - I used Open Weather Map to search for weather data by city name
+        </li>
+      </ul>
+   </div>
+ ); 
+};
 
 export default About;
